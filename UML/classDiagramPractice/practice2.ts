41c7d22dd3f7d4aa27af3af1f35678e2fcaaabfa
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-02-01 15:55:47 +0000

    Basics of UML

---
+++ UML/classDiagramPractice/practice2.ts
@@ -0,0 +1,34 @@
+// クラス図 演習2
+
+interface Shape {
+    calcArea(): number;
+}
+
+class Rectangle implements Shape {
+    private width: number;
+    private height: number;
+
+    calcArea(): number {
+        return this.width * this.height;
+    }
+}
+
+class Circle implements Shape {
+    private radius: number;
+
+    calcArea(): number {
+        return this.radius * this.radius * Math.PI
+    }
+}
+
+class Client {
+    private shape: Shape;
+
+    constructor(shape: Shape) {
+        this.shape = shape;
+    }
+
+    calcArea(): number {
+        return this.shape.calcArea();
+    }
+}
